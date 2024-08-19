4a7102c95d13b4ae1fe9f9cd65e88b2ecb885675
Sayan Bhattacharyya <sn1f3rt@outlook.com>
2024-08-20 01:16:59 +0530

    feat: add `revoke` command

--- cogs/verification.py
+++ cogs/verification.py
@@ -2,12 +2,17 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 import discord
+from discord import app_commands
 from discord.ext import commands
 
+from utils.cd import cooldown
+from utils.tools import is_admin
 from utils.buttons import VerifyButton
 
 if TYPE_CHECKING:
     from bot import RoboNerva
+
+from config import COMMUNITY_GUILD_ID
 
 
 class Verification(commands.Cog):
@@ -29,6 +34,45 @@ class Verification(commands.Cog):
             f"<#{self.bot.config.RULES_CHANNEL_ID}>, and then press the button below "
             f"in order to get verified and have access to the rest of the server.",
             view=view,
+        )
+
+    @app_commands.command(name="revoke")
+    @app_commands.guilds(COMMUNITY_GUILD_ID)
+    @app_commands.checks.dynamic_cooldown(cooldown)
+    async def _revoke(self, ctx: discord.Interaction, member: discord.Member):
+        """(ADMIN) Revoke a user's verification.
+
+        Parameters
+        ----------
+        member : discord.Member
+            The member to revoke verification from.
+
+        """
+        # noinspection PyUnresolvedReferences
+        await ctx.response.defer(thinking=True)
+
+        if not is_admin(ctx.user):
+            return await ctx.edit_original_response(
+                content="Only admins can use this command."
+            )
+
+        verified_user_role = ctx.guild.get_role(
+            self.bot.config.VERIFIED_USER_ROLE_ID
+        )
+        unverified_user_role = ctx.guild.get_role(
+            self.bot.config.UNVERIFIED_USER_ROLE_ID
+        )
+
+        if verified_user_role not in member.roles:
+            return await ctx.edit_original_response(
+                content=f"{member.mention} is not verified."
+            )
+
+        await member.remove_roles(verified_user_role)
+        await member.add_roles(unverified_user_role)
+
+        await ctx.edit_original_response(
+            content=f"{member.mention}'s verification has been revoked."
         )
 
 
