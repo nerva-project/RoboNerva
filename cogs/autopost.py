cfafb566181a1b83e3dab38b49929d1c27859794
Sayan Bhattacharyya <sn1f3rt@outlook.com>
2024-08-28 23:14:42 +0530

    autopost: temporarily suspend tweet lookup until tg integration

--- cogs/autopost.py
+++ cogs/autopost.py
@@ -4,7 +4,8 @@ from typing import TYPE_CHECKING
 from datetime import datetime, time, UTC
 
 import aiohttp
-import twikit
+
+# import twikit
 from dateutil.parser import parse
 
 import discord
@@ -60,6 +61,7 @@ class AutoPost(commands.Cog):
             embed.description += "\n"
 
         else:
+            """
             try:
                 client = twikit.Client("en-US")
 
@@ -80,6 +82,9 @@ class AutoPost(commands.Cog):
             embed.description = (
                 f"Interact on X:\n\nhttps://x.com/NervaCurrency/status/{post_id}\n\n"
             )
+            """
+
+            return
 
         embed.description += (
             "Search for crypto related tweets and plug Nerva where appropriate "
